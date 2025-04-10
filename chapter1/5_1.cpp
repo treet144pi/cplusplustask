1f8df08d8d2f7e5057a2092f136841d9bec8faea
treet <gafarovt685@gmail.com>
2025-04-11 01:54:50 +0300

    last

---
+++ chapter1/5_1.cpp
@@ -0,0 +1,10 @@
+#include <ctype.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    char symbol_check;
+    cin >> symbol_check;
+    cout << islower(symbol_check)<<endl;
+    return 0;
+}
